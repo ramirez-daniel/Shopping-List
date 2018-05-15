b0b2878d87ffc982f5e35f5036b989a177503f75
Daniel A Ramirez <dr82913@srcs.loc>
2018-05-15 12:18:39 -0700

    Made variables

--- listApp.js
+++ listApp.js
@@ -1,4 +1,6 @@
 var button1 = docmument.getElementById("add");
+var inputField = document.getElementById("new");
+var list = document.getElementById("changingList")
 
 function addItem(item) {
     var
